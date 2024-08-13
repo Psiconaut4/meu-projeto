2389997c815255abfafa81ec7ebda8921b48afaf
Nathan Gabriel <168688704+Psiconaut4@users.noreply.github.com>
2024-08-13 16:27:21 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Olá React!!!
+          Olá React!!! tamo ai
         </p>
         <a
           className="App-link"
